a101aaccf18f0458a774817b1ce5019392d76bce
Ehsan <ehsan.aryan2001@gmail.com>
2023-01-26 22:09:07 +0330

    The second commit.

---
+++ dist/js/main.js
@@ -0,0 +1,35 @@
+const menuTrigger = document.getElementById("menu-trigger");
+const navBase = document.getElementById("hidden-nav-base");
+const navBox = document.getElementById("hidden-nav-box");
+
+const showHiddenNav = () => {
+    navBase.style.display = "block";
+    setTimeout(() => {
+        navBase.classList.add("opacity-1");
+        navBox.classList.add("left-0");
+    }, 10);
+};
+
+const hideHiddenNav = () => {
+    navBase.classList.remove("opacity-1");
+    navBox.classList.remove("left-0");
+    setTimeout(() => {
+        navBase.style.display = "none";
+    }, 500);
+};
+
+menuTrigger.addEventListener("click", showHiddenNav);
+
+navBase.addEventListener("click", hideHiddenNav);
+
+navBox.addEventListener("click", (event) => {
+    event.stopPropagation();
+});
+
+addEventListener("resize", () => {
+    if(window.innerWidth > 768) {
+        if(navBase.classList.contains("opacity-1")) {
+            hideHiddenNav();
+        }
+    }
+});
